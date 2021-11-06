9bcc2c4fbc42b1a2816c31c0b622866b77b01001
Trevor Schupbach <trevorschupbach@gmail.com>
2021-11-06 12:38:20 -0400

    TARGET_FPS macro
    cool macro

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 #include "game.h"
 #include "render.h"
 
+#define TARGET_FPS 144
+
 GLFWwindow* window;
 
 int main()
@@ -66,10 +68,10 @@ int main()
 
         glfwSwapBuffers(window);
 
-        double expectedFrame = (nowTime - frameTimer) * globals::target_fps;
+        double expectedFrame = (nowTime - frameTimer) * TARGET_FPS;
         if (frames > expectedFrame)
         {
-            long fix = floor((frames - expectedFrame) * (1000.0 / globals::target_fps));
+            long fix = floor((frames - expectedFrame) * (1000.0 / TARGET_FPS));
             std::this_thread::sleep_for(std::chrono::milliseconds(fix));
         }
 
