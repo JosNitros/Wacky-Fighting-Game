954fb6c9fd4206c0f3f89465f81fb27e18f30bc6
JosNitros <57300674+JosNitros@users.noreply.github.com>
2021-11-06 22:05:51 -0400

    Made boxes

--- fighter.h
+++ fighter.h
@@ -1,11 +1,19 @@
 #include <stdlib.h>
 #include <glm/glm.hpp>
 
+struct box {
+	glm::vec2 positions;
+	glm::vec2 dimensions;
+	box(glm::vec2 startingPos, glm::vec2 dim) : positions(startingPos), dimensions(dim) {};
+};
+
 struct fighter {
 	//position
-	glm::vec2 position;
-	glm::vec2 dimensions;
+	glm::vec2 startingposition;
+	box hitbox = box(startingposition, glm::vec2(0.0f, 0.0f));
+	box hurtbox = box(startingposition, glm::vec2(10.0f, 10.0f));
 	glm::vec2 velocity = glm::vec2(10.0f, 10.0f);
+
 
 	//states
 	bool idle = true;
@@ -24,10 +32,11 @@ struct fighter {
 	bool parrying = false;
 	bool grabbing = false;
 
-	fighter(glm::vec2 startingPos, glm::vec2 dimAnal) : position(startingPos), dimensions(dimAnal) {};
+	fighter(glm::vec2 startingposition) : startingposition(startingposition){};
 
 	void move() {
-		position += velocity;
+		hitbox.positions += velocity;
+		hurtbox.positions += velocity;
 	}
 
 	void setAllFalse() {
